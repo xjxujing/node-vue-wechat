5536472b457e0f79cab30bd1544c6fb939c65f57
xjxujing <xjxujing_xj@foxmail.com>
2020-04-17 09:52:12 +0800

    搭建登录和注册路由接口

--- server.js
+++ server.js
@@ -2,7 +2,13 @@ const express = require("express");
 const mongoose = require("mongoose");
 const app = express();
 
+// 引入 users.js
+const users = require("./routes/api/users");
+
+// db config
 const db = require("./config/keys").mongoURI;
+
+// connect to mongodb
 mongoose.connect(db, { useNewUrlParser: true, useUnifiedTopology: true }).then(
     () => console.log("MonggoDB Connected"),
     (err) => console.log(err)
@@ -11,6 +17,9 @@ mongoose.connect(db, { useNewUrlParser: true, useUnifiedTopology: true }).then(
 app.get("/", (req, res) => {
     res.send("Hello node!");
 });
+
+// 使用 routes
+app.use("/api/users", users);
 
 const port = process.env.PORT || 5000;
 
