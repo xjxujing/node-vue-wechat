17d43e43fa01b861958530576afa45c84e26d091
xjxujing <xjxujing_xj@foxmail.com>
2020-04-17 14:43:32 +0800

    add a comment

--- routes/api/users.js
+++ routes/api/users.js
@@ -7,6 +7,7 @@ const router = express.Router();
  * @desc 返回的请求的 json 数据
  * @access public
  */
+// Router.get() 方法来响应特定路径的 HTTP GET 请求
 router.get("/test", (req, res) => {
     res.json({ msg: "login works" });
 });
